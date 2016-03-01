fcf59720aa030c5d602f4dac2da8ec8a537dcdc7
Johann Felipe González Ávila <jf.gonzalez695@gmail.com>
2016-02-29 20:57:53 -0500

    Probando Comits

--- app/src/main/java/cam/grupo09/gopart_y/activities/MainActivity.java
+++ app/src/main/java/cam/grupo09/gopart_y/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package cam.grupo09.gopart_y;
+package cam.grupo09.gopart_y.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import cam.grupo09.gopart_y.R;
 
 public class MainActivity extends AppCompatActivity {
 
